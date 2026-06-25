6d02d0cc7940b726b469e62b9898ded265d0620e
Sajith Bandara <140834328+sajith23420@users.noreply.github.com>
2026-06-25 18:24:38 +0530

    modify

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -15,8 +15,8 @@ const Contact = () => {
         <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className='font-semibold text-xl text-gray-600'>Our Store</p>
-          <p className='text-gray-500'>77A Bleecker Street,<br /> Greenwich Village, New York City</p>
-          <p className='text-gray-500'>Phone: (123) 456-7890 <br />Email: info@ourstore.com</p>
+          <p className='text-gray-500'>No 18/B Wasanakanda Road,<br /> Katugastota, Kandy</p>
+          <p className='text-gray-500'>Phone: +94 715514658 <br />Email: sajithbandara23420@gmail.com</p>
           <p className='font-semibold text-xl text-gray-600'>Careers at Forever </p>
           <p className='text-gray-500'>Learn more about our teams and job openings</p>
 
@@ -26,7 +26,7 @@ const Contact = () => {
 
       </div>
 
-      <NewsletterBox/>
+      <NewsletterBox />
 
     </div>
   )
